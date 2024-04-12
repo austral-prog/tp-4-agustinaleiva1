2437f7e13bb82abd98f3e1f83c29eb383842146c
agustinaleiva1 <162721976+agustinaleiva1@users.noreply.github.com>
2024-04-12 10:56:52 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,12 @@
 def leap_year():
     print("TO DO")
+    year = input("ingrese un año:")
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print (f"El año {year} es bisiesto")
+    elif year % 4 == 0:
+        print (f"El año {year} es bisiesto")
+    else:
+        print (f"El año {year} no es bisiesto")
+        
+ 
